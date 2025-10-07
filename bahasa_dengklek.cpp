de04d0f0314c8f12a6606aba8bf92384d6c4963c
Michael <michaelyordanis27@gmail.com>
2025-10-07 08:40:27 +0700

    First problem
    https://tlx.toki.id/problems/toki-3/B

---
+++ bahasa_dengklek.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+string input;
+cin >> input;
+for(int i=0; i<input.length(); i++) {
+    if(input[i]>='A' && input[i]<= 'Z'){
+        input[i] += 32;
+    }
+    else if (input[i]>='a' && input[i]<= 'z') {
+        input[i] -= 32;
+    }
+}
+ cout << input;
+return 0;
+}
